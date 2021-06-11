1a905a5d02d7a22eca21030e9dba71ca71e41c8b
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-11 08:42:47 -0600

    Initial Commit

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = "Lord of the Rings"
+is_entertaining = true
+
+movie = "Batman"
+num_4 = 33
+num_6 = 28
+product = num_4 * num_6
+
+favorite_foods = ["Tacos", "Burritos", "Enchiladas"]
+favorite_foods.delete(0)
+
+puts "There are #{movie.length} characters in this variable ID!"
