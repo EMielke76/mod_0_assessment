9706b12a1311121bb7654dec5c5dd1f531362da2
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-11 09:02:50 -0600

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  "type" => "Velociraptor",
+  "eats" => "anything that moves",
+  "height_in_cm" => 985,
+  "weight_in_lbs" => 285,
+  "exists_today" => false,
+  "is_clever_girl" => true,
+  "favorite_snacks" => ["Hunters", "Small Children", "Slow Paleontologists"],
+  "least_favorite_things" => ["Shotguns", "Reflective surfaces", "Tyrannosaurus Rex"]
+}
