ecab874ab4cbe2e58f19c8af579bda62f44bc252
Eric Mielke <BeardBikesandBeers@gmail.com>
2021-06-11 09:01:56 -0600

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer = {
+  "name" => "Eric Mielke",
+  "programming_language" => "Ruby",
+  "years_writing_code" => 0.25,
+  "age_in_years" => 33,
+  "loves_programing" => true,
+  "is_ready_for_mod_1" => true,
+  "favorite_foods" => ["Tacos", "Burritos", "Enchiladas"],
+  "favorite_drinks" => ["Palomas", "coffee", "H20"]
+}
